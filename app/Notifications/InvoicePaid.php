d4fbbd9e2086b92d7f9a130a5f997ae4bd9f624b
Daniel Ponce <dmpb019@gmail.com>
2019-08-02 23:52:54 -0500

    Notificaciones por mail

--- app/Notifications/InvoicePaid.php
+++ app/Notifications/InvoicePaid.php
@@ -34,7 +34,7 @@ class InvoicePaid extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['mail'];
     }
 
     /**
@@ -46,9 +46,8 @@ class InvoicePaid extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                ->subject('Notification test')
+                ->markdown('mail.mailtrap');
     }
 
     /**
